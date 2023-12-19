db10d94cb783304c45a0da486287d7b6978c6429
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2023-12-19 13:21:03 +0000

    Adding about me details to data

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -11,32 +11,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-  skills?: { name:string, exp:number }[];
+  skills: { name:string, exp:number }[] = [];
 
   ngOnInit() {
-    this.skills = [
-      { name:"ASP.NET Core", exp:1 },
-      { name:"TypeScript", exp:2 },
-      { name:"jQuery", exp:3 },
-      { name:"React", exp:40 },
-      { name:"JavaScript", exp:5 },
-      { name:"NoSQL (MongoDB)", exp:80 },
-      { name:"Ionic Framework", exp:85 },
-      { name:"Bootstrap", exp:100 },
-      { name:"Angular", exp:50 },
-      { name:"HTML5", exp:30 },
-      { name:"Entity Framework", exp:41 },
-      { name:"SASS", exp:6 },
-      { name:"Git", exp:7 },
-      { name:"PHP", exp:70 },
-      { name:"RESTful APIs", exp:15 },
-      { name:"SQL (SQL Server, MySQL, PostgreSQL)", exp:10 },
-      { name:"CSS3", exp:0 },
-      { name:"C#", exp:90 },
-      { name:"Laravel", exp:60 },
-      { name:"Python", exp:50 },
-      { name:"SCSS", exp:45 },
-      { name:"WordPress", exp:35 },
-    ].sort((a , b) => a.exp > b.exp ? -1 : 1);
+    this.skills.sort((a , b) => a.exp > b.exp ? -1 : 1);
   }
 }
