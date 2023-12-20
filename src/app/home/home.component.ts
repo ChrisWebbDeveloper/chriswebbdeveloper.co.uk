64a6db71e0440617f94bcfac505d909fd83d335d
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2023-12-20 12:59:42 +0000

    Adding home content to page dynamically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { HomeDetails } from '../models/home-details';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  @Input() details? : HomeDetails;
+  @Input() details!: HomeDetails;
 
   ngOnInit() {
     console.log("Home", this.details);
--- src/app/models/content.ts
+++ src/app/models/content.ts
@@ -7,9 +7,9 @@ export class Content {
     contactDetails: {};
 
     constructor(content: { home_details: any, about_details: any, projects_details: any, contact_details: any}) {
-        this.homeDetails = new HomeDetails(content.home_details);
-        this.aboutDetails = content.about_details;
-        this.projectsDetails = content.projects_details;
-        this.contactDetails = content.contact_details;
+        this.homeDetails = new HomeDetails(content.home_details ?? {});
+        this.aboutDetails = content.about_details ?? {};
+        this.projectsDetails = content.projects_details ?? {};
+        this.contactDetails = content.contact_details ?? {};
     }
 }
