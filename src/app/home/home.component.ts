d03048b94bd47058a4378a00f62804d10c707e58
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2023-12-20 13:58:44 +0000

    Updating version no

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { HomeDetails } from '../models/home-details';
 import { NgFor, NgIf } from '@angular/common';
 import { Contacts } from '../models/contacts';
+import { version } from '../../../package.json';
 
 @Component({
   selector: 'app-home',
@@ -16,6 +17,7 @@ import { Contacts } from '../models/contacts';
 export class HomeComponent {
   @Input() details!: HomeDetails;
   @Input() contacts!: Contacts;
+  version: string = version;
 
   ngOnInit() {
     console.log("Home", this.details);
