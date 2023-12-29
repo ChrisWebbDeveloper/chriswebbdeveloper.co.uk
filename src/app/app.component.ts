7e8e068ec6c3e9c9d42a50e0d46105225c1b256a
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2023-12-29 18:42:14 +0000

    Updating app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
-import { Content } from './models/content';
 import { FooterComponent } from './footer/footer.component';
 import { DataService } from './services/data.service';
+import { Data } from './models/data';
+import { AboutDetails } from './models/about-details';
+import { HomeDetails } from './models/home-details';
+import { Skill } from './models/skill';
+import { Project } from './models/project';
+import { ContactDetails } from './models/contact-details';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet,
     HeaderComponent,
     HomeComponent,
     AboutMeComponent,
@@ -27,14 +30,24 @@ import { DataService } from './services/data.service';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  content?: Content;
+  homeDetails!: HomeDetails;
+  aboutDetails!: AboutDetails;
+  skills: Skill[] = [];
+  projects: Project[] = [];
+  contactDetails!: ContactDetails;
   
   constructor(private data: DataService) {
   }
 
   ngOnInit() {
     this.data.getData().subscribe({
-      next: val => this.content = new Content(val),
+      next: (val: Data) => {
+        this.homeDetails = new HomeDetails(val.name, val.blurb);
+        this.aboutDetails = new AboutDetails(val.about_me.long_form, val.title);
+        this.skills = val.skills.map(skill => new Skill(skill));
+        this.projects = val.projects.map(project => new Project(project));
+        this.contactDetails = new ContactDetails(val.contact_details);
+      },
       error: err => alert("Content could not be found at this time. Please try again later.")
     });
   }
