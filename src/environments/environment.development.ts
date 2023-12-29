982e591c16ed94e36329aa628b04d4d0474cd095
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2023-12-29 19:27:58 +0000

    Updating environments to pull in info

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-    dataUrl: "../assets/data/data.json"
+    dataUrl: "https://raw.githubusercontent.com/ChrisWebbDeveloper/cv/main/christopher-webb-cv.json",
+    docxCv: "https://github.com/ChrisWebbDeveloper/cv/raw/main/Christopher%20Webb%20-%20CV.docx",
+    pdfCv: "https://github.com/ChrisWebbDeveloper/cv/raw/main/Christopher%20Webb%20-%20CV.pdf"
 };
