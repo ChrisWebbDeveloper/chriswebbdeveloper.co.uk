f8be8ef6c5eb9f3ede8d50b3c161504d3b6599b1
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2023-12-30 15:07:01 +0000

    Add cv tests

--- src/app/models/cv.spec.ts
+++ src/app/models/cv.spec.ts
@@ -1,7 +1,47 @@
 import { Cv } from './cv';
 
 describe('Cv', () => {
-  it('should create an instance', () => {
-    expect(new Cv()).toBeTruthy();
-  });
+    let cv: Cv;
+    const pdf = 'pdf';
+    const docx = 'docx';
+
+    beforeEach(async () => {
+        cv = new Cv({pdf: pdf, docx: docx});
+    });
+
+    describe(`Component`, () => {
+        it(`should create an instance`, () => {
+            expect(cv).toBeTruthy();
+        });
+    });
+
+    describe(`pdf`, () => {
+        it('should be included as a property', () => {
+            expect(cv.pdf).toBeTruthy();
+        });
+
+        it('should be undefined if not set', () => {
+            cv = new Cv({});
+            expect(cv.pdf).toBeUndefined();
+        });
+
+        it('should be set to the value provided in the constructor', () => {
+            expect(cv.pdf).toEqual(pdf);
+        });
+    });
+
+    describe(`docx`, () => {
+        it('should be included as a property', () => {
+            expect(cv.docx).toBeTruthy();
+        });
+
+        it('should be undefined if not set', () => {
+            cv = new Cv({});
+            expect(cv.docx).toBeUndefined();
+        });
+
+        it('should be set to the value provided in the constructor', () => {
+            expect(cv.docx).toEqual(docx);
+        });
+    });
 });
