b225456e2efedd2bd85dc65a52f12b22f244d4d0
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2023-12-31 10:59:32 +0000

    Adding HomeDetails tests

--- src/app/models/home-details.spec.ts
+++ src/app/models/home-details.spec.ts
@@ -1,7 +1,42 @@
 import { HomeDetails } from './home-details';
 
 describe('HomeDetails', () => {
-  it('should create an instance', () => {
-    expect(new HomeDetails()).toBeTruthy();
-  });
+    let homeDetails: HomeDetails;
+    const name = 'Name';
+    const blurb = 'Blurb';
+
+    beforeEach(async () => {
+        homeDetails = new HomeDetails(name, blurb);
+    });
+
+    describe(`Component`, () => {
+        it(`should create an instance`, () => {
+            expect(homeDetails).toBeTruthy();
+        });
+    });
+
+    describe(`name`, () => {
+        it('should be included as a property', () => {
+            expect(homeDetails.name).toBeTruthy();
+        });
+
+        it('should be set to the value provided in the constructor', () => {
+            expect(homeDetails.name).toEqual(name);
+        });
+    });
+
+    describe(`blurb`, () => {
+        it('should be included as a property', () => {
+            expect(homeDetails.blurb).toBeTruthy();
+        });
+
+        it('should be undefined if not set', () => {
+            homeDetails = new HomeDetails(name);
+            expect(homeDetails.blurb).toBeUndefined();
+        });
+
+        it('should be set to the value provided in the constructor', () => {
+            expect(homeDetails.blurb).toEqual(blurb);
+        });
+    });
 });
