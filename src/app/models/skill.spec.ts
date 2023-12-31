a080d75bda76d2d3d0c7a8ee224d14e85908aa2b
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2023-12-31 13:51:54 +0000

    Adding Skill testing

--- src/app/models/skill.spec.ts
+++ src/app/models/skill.spec.ts
@@ -1,7 +1,52 @@
 import { Skill } from './skill';
 
-describe('Skill', () => {
-  it('should create an instance', () => {
-    expect(new Skill()).toBeTruthy();
-  });
+describe(`AboutDetails`, () => {
+    let skill: Skill;
+    const name: string = 'Name';
+    const exp: number = 50;
+    let skillObj: {
+        name: string,
+        exp?: number | null
+    }
+
+    beforeEach(async () => {
+        skillObj = {name, exp};
+        skill = new Skill(skillObj)
+    });
+
+    describe(`Component`, () => {
+        it(`should create an instance`, () => {
+            expect(skill).toBeTruthy();
+        });
+    });
+
+    describe(`name`, () => {
+        it(`should be included as a property`, () => {
+            expect(skill.name).toBeTruthy();
+        });
+
+        it(`should match the given value passed into the constructor if provided`, () => {
+            expect(skill.name).toEqual(name);
+        });
+    });
+
+    describe(`exp`, () => {
+        it(`should be included as a property`, () => {
+            expect(skill.exp).toBeTruthy();
+        });
+
+        it(`should match the given value passed into the constructor if provided`, () => {
+            expect(skill.exp).toEqual(exp);
+        });
+
+        it(`should be set to 0 if a value is not passed into the constructor, or the value passed in is null`, () => {
+            skillObj.exp = undefined;
+            skill = new Skill(skillObj);
+            expect(skill.exp).toEqual(0);
+
+            skillObj.exp = null;
+            skill = new Skill(skillObj);
+            expect(skill.exp).toEqual(0);
+        });
+    });
 });
